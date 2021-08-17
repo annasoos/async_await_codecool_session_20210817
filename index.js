399f35bd0e39ca93cd2eab4dd5d73fa38d843acf
Annamária Soós <annasoos94@gmail.com>
2021-08-17 09:37:25 +0200

    write async fetch example

---
+++ index.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+const run = async () => {
+
+	const x = 5;
+	const y = "Anna";
+
+	const myFunc = (num) => {
+		const res = num + 5
+		console.log(res)
+	};
+
+	const dataFromServer = await fetch("https://rickandmortyapi.com/api/character");
+	const data = await dataFromServer.json();
+	console.log(data);
+
+	console.log(y);
+	myFunc(1);
+	console.log("ez utóbb jelenik meg")
+
+};
+
+const resultOfAsync = run();
+
+console.log(resultOfAsync);
+
+console.log("ez előbb jelenik meg");
